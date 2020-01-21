b000d7748ecded3bcbe944f49ed009842b0063f5
Frost-Bitten <60068449+Frost-Bitten@users.noreply.github.com>
2020-01-21 19:53:52 +0300

    Add files via upload

--- main_one.py
+++ main_one.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -8,11 +9,26 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 updater = Updater(token='1048309903:AAFvA1djwosUF-MnZLR39G6LweAQl80c328', use_context='true')
 dispatcher = updater.dispatcher
 # start_handler = CommandHandler('start', start)
-def startMe(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-start_handler = CommandHandler('start', startMe)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please kiss me! 💋 💋 💋")
+
+
+def end(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="SO FUCK OFF !!! 🖕🏻 🖕🏻 🖕🏻")
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+start_handler = CommandHandler('start', start)
+end_handler = CommandHandler('end', end)
+echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(end_handler)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
 
